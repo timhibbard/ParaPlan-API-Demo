04cac258bfedb6f30fc8e51e884ae48a09a001e4
timhibbard@gmail.com <timhibbard@gmail.com>
2016-08-18 13:19:14 -0400

    First commit

--- ParaPlan-API-Demo/MainWindow.xaml.vb
+++ ParaPlan-API-Demo/MainWindow.xaml.vb
@@ -1,3 +1,54 @@
-﻿Class MainWindow
+﻿Imports System.Net
+Imports Newtonsoft
+Imports Newtonsoft.Json
+
+
+Class MainWindow
+
+    'ReadOnly api As String = "https://aws.engraph.com/REST/SFCS/"
+    ReadOnly api As String = "https://aws.engraph.com/REST/SFCSTEST/"
+    Public token As String
+    Public kimPW As String = "6637AA99E18177E7663960496752E46A3CCE8012FB8E4D599DF0890EE3C1A299C768CE89FB2C300CB680A1B22FA98E0AB126049EE703AB92B0D8EBEBB99F3CFF"
+    Public kimEmail As String = "kim.marsh@st-francis.org"
+
+
+
+    Function GetTrips() As List(Of Trip)
+        If String.IsNullOrWhiteSpace(token) Then
+            token = GetToken()
+        End If
+
+        Dim request As WebRequest = WebRequest.Create(api + $"TripService/Trips?Token={token}&Device=APIDEMO&Driver=31&Date=2016-08-15")
+
+
+
+
+        Return New List(Of Trip)
+
+    End Function
+
+    Function GetToken() As String
+        'https://aws.engraph.com/ParaPlanREST/UserService/Login?UserName=kim.marsh@st-francis.org&Password=6637AA99E18177E7663960496752E46A3CCE8012FB8E4D599DF0890EE3C1A299C768CE89FB2C300CB680A1B22FA98E0AB126049EE703AB92B0D8EBEBB99F3CFF&Device=APIDEMO&Version=0.1&DeviceToken=
+        Dim request As WebRequest = WebRequest.Create($"https://aws.engraph.com/ParaPlanREST/UserService/Login?UserName={kimEmail}&Password={kimPW}&Device=APIDEMO&Version=0.1&DeviceToken=")
+
+        Return String.Empty
+    End Function
+
+
+
 
 End Class
+
+Public Class Trip
+    Public tripID As Int32
+    Public client As Client
+
+End Class
+
+Public Class Client
+    Public clientID As Int32
+    Public firstName As String
+    Public lastName As String
+
+End Class
+
