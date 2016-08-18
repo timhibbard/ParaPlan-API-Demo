d705833b29e544af27758dc0b53e0f1bf9397c3b
timhibbard@gmail.com <timhibbard@gmail.com>
2016-08-18 14:08:26 -0400

    Shows how to pull trip data from API

--- ParaPlan-API-Demo/MainWindow.xaml.vb
+++ ParaPlan-API-Demo/MainWindow.xaml.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Net
+﻿Imports System.IO
+Imports System.Net
 Imports Newtonsoft
 Imports Newtonsoft.Json
 
@@ -19,8 +20,29 @@ Class MainWindow
         End If
 
         Dim request As WebRequest = WebRequest.Create(api + $"TripService/Trips?Token={token}&Device=APIDEMO&Driver=31&Date=2016-08-15")
+        request.ContentType = "application/json; charset=utf-8"
+        listResults.Items.Add(request.RequestUri.ToString())
+
+        Try
+            Using response As HttpWebResponse = request.GetResponse()
+                Dim reader As StreamReader = New StreamReader(response.GetResponseStream)
+                Dim s As String = reader.ReadToEnd()
+                listResults.Items.Add(s)
+                Dim trips As List(Of Trip) = JsonConvert.DeserializeObject(Of List(Of Trip))(s)
+
+                For Each t In trips
+                    listResults.Items.Add(t.tripID)
+
+                Next
 
 
+
+
+            End Using
+
+        Catch ex As Exception
+            listResults.Items.Add(ex.ToString())
+        End Try
 
 
         Return New List(Of Trip)
@@ -30,13 +52,33 @@ Class MainWindow
     Function GetToken() As String
         'https://aws.engraph.com/ParaPlanREST/UserService/Login?UserName=kim.marsh@st-francis.org&Password=6637AA99E18177E7663960496752E46A3CCE8012FB8E4D599DF0890EE3C1A299C768CE89FB2C300CB680A1B22FA98E0AB126049EE703AB92B0D8EBEBB99F3CFF&Device=APIDEMO&Version=0.1&DeviceToken=
         Dim request As WebRequest = WebRequest.Create($"https://aws.engraph.com/ParaPlanREST/UserService/Login?UserName={kimEmail}&Password={kimPW}&Device=APIDEMO&Version=0.1&DeviceToken=")
+        request.ContentType = "application/json; charset=utf-8"
+        Dim rv As String
+        Try
+            Using response As HttpWebResponse = request.GetResponse()
+                Dim reader As StreamReader = New StreamReader(response.GetResponseStream)
+                Dim s As String = reader.ReadToEnd()
+                listResults.Items.Add(s)
+                Dim user As User = JsonConvert.DeserializeObject(Of User)(s)
 
-        Return String.Empty
+                listResults.Items.Add(user.name)
+
+                rv = user.key
+
+
+            End Using
+
+        Catch ex As Exception
+            listResults.Items.Add(ex.ToString())
+        End Try
+
+        Return rv
+
     End Function
 
-
-
-
+    Private Sub GetTripsButton_Click(sender As Object, e As RoutedEventArgs)
+        GetTrips()
+    End Sub
 End Class
 
 Public Class Trip
@@ -49,6 +91,13 @@ Public Class Client
     Public clientID As Int32
     Public firstName As String
     Public lastName As String
+
+End Class
+
+Public Class User
+    Public name As String
+    Public userId As String
+    Public key As String
 
 End Class
 
